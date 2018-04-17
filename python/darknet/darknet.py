b832a10702ed9a429eb6d70d8804ba78898d8992
Hakon BG <hakon.grundnes@stingray.no>
2018-04-17 11:15:10 +0200

    Python - fixed module import and python3 compatibility

--- python/darknet/darknet.py
+++ python/darknet/darknet.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from ctypes import *
 import math
 import random
@@ -151,6 +152,6 @@ if __name__ == "__main__":
     net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
     meta = load_meta("cfg/coco.data")
     r = detect(net, meta, "data/dog.jpg")
-    print r
+    print(r) 
     
 
